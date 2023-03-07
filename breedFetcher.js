61fda74d1d8e16b56b3fcd716926a46f54607a95
abdmmn <abduljmemon@gmail.com>
2023-03-07 19:31:03 +0000

    cat stuff

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,5 +1,8 @@
 const request = require('request');
-request('https://api.thecatapi.com/v1/breeds/search?q=sib', function (error, response, body) {
+
+const URL = `https://api.thecatapi.com/v1/breeds/search?q=${process.argv[2]}`;
+
+request(URL, function (error, response, body) {
   //console.error('error:', error); // Print the error if one occurred
   //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   console.log('body:', body); // Print the HTML for the Google homepage.
@@ -12,3 +15,5 @@ console.log(typeof data);
 });
 
 // this line not working >>>>>>>>>> console.log(data.weight)
+
+//'https://api.thecatapi.com/v1/breeds/search?q=sib'
